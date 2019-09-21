3e58bb810c2841cda4d5e0c1ff24d3e403b9decb
Oleg Shabarshin <oleg_shabarshin@mail.ru>
2019-09-22 00:00:45 +0300

    Заглушка Brush, Flood

--- CompGraph_lab3/FormEx1.cs
+++ CompGraph_lab3/FormEx1.cs
@@ -13,6 +13,7 @@ namespace CompGraph_lab3
     public partial class FormEx1 : Form
     {
         Form FormMain;
+        short SelectedTypeOfPainting = 0;
         public FormEx1(Form formMain)
         {
             InitializeComponent();
@@ -30,6 +31,21 @@ namespace CompGraph_lab3
         }
 
         private void PictureBoxFloodingArea_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (SelectedTypeOfPainting == 0)
+            {
+                BrushArea(sender, e);
+            }
+            else
+                FloodArea(sender, e);
+        }
+
+        void BrushArea(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        void FloodArea(object sender, MouseEventArgs e)
         {
 
         }
