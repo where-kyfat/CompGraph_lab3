dda7e1dd4c0d65bdcdacecd52d87426c3d85ab57
vitkazak <31851694+vitkazak@users.noreply.github.com>
2019-09-22 02:02:10 +0300

    временный 1

--- CompGraph_lab3/FormEx1.Designer.cs
+++ CompGraph_lab3/FormEx1.Designer.cs
@@ -44,18 +44,24 @@
             // 
             // pictureBoxFloodingArea
             // 
-            this.pictureBoxFloodingArea.Location = new System.Drawing.Point(34, 47);
+            this.pictureBoxFloodingArea.Location = new System.Drawing.Point(45, 58);
+            this.pictureBoxFloodingArea.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBoxFloodingArea.Name = "pictureBoxFloodingArea";
-            this.pictureBoxFloodingArea.Size = new System.Drawing.Size(459, 315);
+            this.pictureBoxFloodingArea.Size = new System.Drawing.Size(612, 388);
             this.pictureBoxFloodingArea.TabIndex = 0;
             this.pictureBoxFloodingArea.TabStop = false;
+            this.pictureBoxFloodingArea.Click += new System.EventHandler(this.PictureBoxFloodingArea_Click);
             this.pictureBoxFloodingArea.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBoxFloodingArea_MouseClick);
+            this.pictureBoxFloodingArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxFloodingArea_MouseDown);
+            this.pictureBoxFloodingArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBoxFloodingArea_MouseMove);
+            this.pictureBoxFloodingArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxFloodingArea_MouseUp);
             // 
             // button_colorDialogEx1
             // 
-            this.button_colorDialogEx1.Location = new System.Drawing.Point(734, 47);
+            this.button_colorDialogEx1.Location = new System.Drawing.Point(979, 58);
+            this.button_colorDialogEx1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_colorDialogEx1.Name = "button_colorDialogEx1";
-            this.button_colorDialogEx1.Size = new System.Drawing.Size(104, 23);
+            this.button_colorDialogEx1.Size = new System.Drawing.Size(139, 28);
             this.button_colorDialogEx1.TabIndex = 1;
             this.button_colorDialogEx1.Text = "Выбрать цвет";
             this.button_colorDialogEx1.UseVisualStyleBackColor = true;
@@ -63,18 +69,20 @@
             // 
             // pictureBoxCorrectColor
             // 
-            this.pictureBoxCorrectColor.Location = new System.Drawing.Point(689, 47);
+            this.pictureBoxCorrectColor.Location = new System.Drawing.Point(919, 58);
+            this.pictureBoxCorrectColor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBoxCorrectColor.Name = "pictureBoxCorrectColor";
-            this.pictureBoxCorrectColor.Size = new System.Drawing.Size(22, 23);
+            this.pictureBoxCorrectColor.Size = new System.Drawing.Size(29, 28);
             this.pictureBoxCorrectColor.TabIndex = 2;
             this.pictureBoxCorrectColor.TabStop = false;
             // 
             // labelCorrectColor
             // 
             this.labelCorrectColor.AutoSize = true;
-            this.labelCorrectColor.Location = new System.Drawing.Point(582, 52);
+            this.labelCorrectColor.Location = new System.Drawing.Point(776, 64);
+            this.labelCorrectColor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCorrectColor.Name = "labelCorrectColor";
-            this.labelCorrectColor.Size = new System.Drawing.Size(101, 13);
+            this.labelCorrectColor.Size = new System.Drawing.Size(132, 17);
             this.labelCorrectColor.TabIndex = 3;
             this.labelCorrectColor.Text = "Выбранный цвет - ";
             // 
@@ -82,9 +90,10 @@
             // 
             this.radioButtonSelectedBrush.AutoSize = true;
             this.radioButtonSelectedBrush.Checked = true;
-            this.radioButtonSelectedBrush.Location = new System.Drawing.Point(604, 99);
+            this.radioButtonSelectedBrush.Location = new System.Drawing.Point(805, 122);
+            this.radioButtonSelectedBrush.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButtonSelectedBrush.Name = "radioButtonSelectedBrush";
-            this.radioButtonSelectedBrush.Size = new System.Drawing.Size(55, 17);
+            this.radioButtonSelectedBrush.Size = new System.Drawing.Size(67, 21);
             this.radioButtonSelectedBrush.TabIndex = 4;
             this.radioButtonSelectedBrush.TabStop = true;
             this.radioButtonSelectedBrush.Text = "Кисть";
@@ -94,9 +103,10 @@
             // radioButtonSelectedFlood
             // 
             this.radioButtonSelectedFlood.AutoSize = true;
-            this.radioButtonSelectedFlood.Location = new System.Drawing.Point(604, 123);
+            this.radioButtonSelectedFlood.Location = new System.Drawing.Point(805, 151);
+            this.radioButtonSelectedFlood.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButtonSelectedFlood.Name = "radioButtonSelectedFlood";
-            this.radioButtonSelectedFlood.Size = new System.Drawing.Size(68, 17);
+            this.radioButtonSelectedFlood.Size = new System.Drawing.Size(84, 21);
             this.radioButtonSelectedFlood.TabIndex = 5;
             this.radioButtonSelectedFlood.TabStop = true;
             this.radioButtonSelectedFlood.Text = "Заливка";
@@ -109,9 +119,10 @@
             // 
             // buttonOpenFile
             // 
-            this.buttonOpenFile.Location = new System.Drawing.Point(734, 217);
+            this.buttonOpenFile.Location = new System.Drawing.Point(979, 267);
+            this.buttonOpenFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonOpenFile.Name = "buttonOpenFile";
-            this.buttonOpenFile.Size = new System.Drawing.Size(104, 23);
+            this.buttonOpenFile.Size = new System.Drawing.Size(139, 28);
             this.buttonOpenFile.TabIndex = 6;
             this.buttonOpenFile.Text = "Открыть файл";
             this.buttonOpenFile.UseVisualStyleBackColor = true;
@@ -122,18 +133,19 @@
             this.comboBoxSelectedFile.FormattingEnabled = true;
             this.comboBoxSelectedFile.Items.AddRange(new object[] {
             "нет"});
-            this.comboBoxSelectedFile.Location = new System.Drawing.Point(604, 217);
+            this.comboBoxSelectedFile.Location = new System.Drawing.Point(805, 267);
+            this.comboBoxSelectedFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxSelectedFile.Name = "comboBoxSelectedFile";
-            this.comboBoxSelectedFile.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSelectedFile.Size = new System.Drawing.Size(160, 24);
             this.comboBoxSelectedFile.TabIndex = 7;
             this.comboBoxSelectedFile.Text = "нет";
             this.comboBoxSelectedFile.SelectedIndexChanged += new System.EventHandler(this.ComboBoxSelectedFile_SelectedIndexChanged);
             // 
             // FormEx1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 507);
+            this.ClientSize = new System.Drawing.Size(1176, 624);
             this.Controls.Add(this.comboBoxSelectedFile);
             this.Controls.Add(this.buttonOpenFile);
             this.Controls.Add(this.radioButtonSelectedFlood);
@@ -143,6 +155,7 @@
             this.Controls.Add(this.button_colorDialogEx1);
             this.Controls.Add(this.pictureBoxFloodingArea);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormEx1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CompGraph_lab3.Ex1";
--- CompGraph_lab3/FormEx1.cs
+++ CompGraph_lab3/FormEx1.cs
@@ -12,12 +12,32 @@ namespace CompGraph_lab3
 {
     public partial class FormEx1 : Form
     {
+        private Image StartImage;
+        private Point prevLocation;
+        bool isDown = false;
+        Graphics g;
+
         Form FormMain;
         short SelectedTypeOfPainting = 0;
         public FormEx1(Form formMain)
         {
             InitializeComponent();
+            g = CreateGraphics();
             FormMain = formMain;
+            Bitmap pictureBoxImage = new Bitmap(pictureBoxFloodingArea.Width, pictureBoxFloodingArea.Height);
+            DrawingEmptyImage(pictureBoxImage);
+            pictureBoxFloodingArea.Image = pictureBoxImage;
+            StartImage = pictureBoxImage;
+        }
+        private void DrawingEmptyImage(Bitmap EmptyImage) {
+            //Bitmap EmptyImage = new Bitmap(pictureBoxFloodingArea.Width, pictureBoxFloodingArea.Height);
+            for (int x = 0; x < EmptyImage.Width; x++)
+            {
+                for (int y = 0; y < EmptyImage.Height; y++)
+                {
+                    EmptyImage.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+            }
         }
 
         private void FormEx1_FormClosing(object sender, FormClosingEventArgs e)
@@ -32,17 +52,21 @@ namespace CompGraph_lab3
 
         private void PictureBoxFloodingArea_MouseClick(object sender, MouseEventArgs e)
         {
-            if (SelectedTypeOfPainting == 0)
-            {
-                BrushArea(sender, e);
-            }
-            else
-                FloodArea(sender, e);
+            
         }
+        
 
         void BrushArea(object sender, MouseEventArgs e)
         {
-
+            
+            Bitmap pixels = new Bitmap(pictureBoxFloodingArea.Image);
+            if (isDown == false)
+            {
+                
+                g.DrawLine(new Pen(Color.Black,4), prevLocation,new PointF(e.X, e.Y));
+                pictureBoxFloodingArea.Image = pixels;
+          
+            }
         }
 
         void FloodArea(object sender, MouseEventArgs e)
@@ -68,6 +92,34 @@ namespace CompGraph_lab3
         private void ComboBoxSelectedFile_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void PictureBoxFloodingArea_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBoxFloodingArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isDown = false;
+                prevLocation = e.Location;
+        }
+
+        private void PictureBoxFloodingArea_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isDown = true;
+        }
+
+        private void PictureBoxFloodingArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (SelectedTypeOfPainting == 0)
+            {
+                BrushArea(sender, e);
+            }
+            else
+                FloodArea(sender, e);
         }
     }
 }
